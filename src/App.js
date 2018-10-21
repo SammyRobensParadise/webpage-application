04140d9f8131ba6dcf7e4e73f4f1e72247839529
SammyRobensParadise <33642354+SammyRobensParadise@users.noreply.github.com>
2018-10-21 14:33:00 -0700

    implement bs4 navigation bar boiler plate template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './Navigation';
 import Landing from './Landing';
 import Filmography from './Filmography';
 import Profile from './Profile';
@@ -11,25 +12,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <Navigation />
       <Landing />
       <Filmography />
       <Profile />
       <Contact />
       <Footer />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
