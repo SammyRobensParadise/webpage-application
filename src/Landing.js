eb15f42842c042e366b2e2932dae9f5e54cbbfe3
SammyRobensParadise <33642354+SammyRobensParadise@users.noreply.github.com>
2018-11-03 13:29:15 -0700

    updating JS for carousel

--- src/Landing.js
+++ src/Landing.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
-import inkstain from './inkstain.svg';
 import Navigation from './Navigation';
 import './Landing.css';
 import './App.css';
 import './inkstain.svg';
+import BullyMasked from './img/BullyMasked.png';
+import NerdyMasked from './img/NerdyMasked.png';
+import FancyMasked from './img/FancyMasked.png';
+import StunningMasked from './img/StunningMasked.png';
 import { Container, Row, Col } from 'reactstrap';
 
 class Landing extends Component {
   constructor(props){
     super(props);
     this.state = { Images:[
-      
+      BullyMasked,
+      NerdyMasked,
+      FancyMasked,
+      StunningMasked
     ],
-    current: 0,
-    isNext: true
+    current: 0
     };
   };
+  getImgNumSize(props){
+      var imgNum = this.state.Images.length;
+      return imgNum;
+    };
+  getCurrentImg(props){
+    return this.setCurrentImg();
+  };
+  setCurrentImg(props){
+    //checking to see if the current value is that of first element in the array
+    var defaultCurrent = this.state.current;
+    var num = this.getImgNumSize()-this.getImgNumSize();
+    if (defaultCurrent === num ){
+      return this.state.Images[num].toString();
+    }
+  };
+  handleNext(props){
+
+  };
   render() {
+    var name = "Emily Bandel";
+    let headerPhoto = <img className="EmilyBandelImg" src={this.getCurrentImg()} alt={name+this.getCurrentImg()}/>
+    //let nextButton = <div className="taverseCarouselbtn" onClick={this.setCurrentImg()} alt="Next Image"></div>
+
     return (
      <div className="inkstain">
       <Navigation />
@@ -32,14 +59,15 @@ class Landing extends Component {
       </Col>
       <Col xs="4"className="heroright">
       <Container>
-
+      {headerPhoto}
       </Container>
       </Col>
       </Row> 
      <Container><Row className="vancouver">
       <Col xs="12"><h2><span>Vancouver, BC</span></h2></Col>
       </Row>
-      </Container> 
+      </Container>
+      
       <Container>
       <Row>
         <Col>
