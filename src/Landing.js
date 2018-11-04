b23d3eb1604defdb07adf310a159200527248b62
SammyRobensParadise <33642354+SammyRobensParadise@users.noreply.github.com>
2018-11-04 13:46:37 -0800

    added

--- src/Landing.js
+++ src/Landing.js
@@ -1,3 +1,11 @@
+/*___________________________________________
+CODE ASSET OF SAMUEL ROBENS-PARADISE © 2018
+DISTRIBUTION OF CODE WITHOUT WRITTEN CONSENT OF
+SAMUEL ROBENS-PARADISE IS PROHIBITED UNLESS
+ACCESSED THROUGH PUBLIC DOIMAIN SUCH AS GITHUB
+OR ANY OTHER OPEN SOURCE PLATFORM ON WHICH ASSET
+WAS PUBLISHED WITH AUTHOR CONSENT
+*/
 import React, { Component } from 'react';
 import Navigation from './Navigation';
 import './Landing.css';
@@ -12,6 +20,7 @@ import StunningMasked from './img/StunningMasked.png';
 import { Container, Row, Col } from 'reactstrap';
 
 class Landing extends Component {
+  //landing constructor...
   constructor(props){
     super(props);
     this.state = { Images:[
@@ -25,6 +34,7 @@ class Landing extends Component {
     };
     this.handleNext =this.handleNext.bind(this);
   };
+  //handles carousel onClick...
    handleNext(){
      let index = this.state.current,
      length  = this.state.Images.length-1;
@@ -38,20 +48,23 @@ class Landing extends Component {
         isNext: true
       });
      }
+  //render function
   render() {
-
+    //create local variable that inherits the current state of class variables...
     let index = this.state.current;
     let isNext= this.state.isNext;
     let src = this.state.Images[index];
-    //generating local variables for use in landing....
+    //generating global variables for use in landing....
     var name = "Emily Bandel";
     var title = "Actress";
     var exploreBtntxt = "Explore"
     var city = "Vancouver";
     var province = "BC";
+    //generating local variables for use in landing...
     let headerPhoto = <img className="EmilyBandelImg" src={src} alt={src.toString()+isNext}/>
     let nextButton = <div className="taverseCarouselbtn" onClick={this.handleNext} alt="Next Image"></div>
     let exploreButton = <button className="explore_Contributions_btn"><h3><span>{exploreBtntxt}</span></h3></button>
+    //return function as follows appears physcially to user
     return (
      <div className="inkstain">
       <Navigation />
