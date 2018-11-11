02626a6cb628b1a8b4238de0fddeb77a2d1d615e
SammyRobensParadise <33642354+SammyRobensParadise@users.noreply.github.com>
2018-11-10 18:00:08 -0800

    added Filmography images and assets

--- src/Filmography.js
+++ src/Filmography.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
-import logo from './assets/logo.svg';
 import './App.css';
+import './Filmography.css';
+import App from './App';
+import './assets/inkstainfilm.svg';
+import { Container, Row, Col } from 'reactstrap';
 
 class Filmography extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      sectionTitle: 'Filmography',
+      sectionDefinition: 'A collection of works, films and screenings ft. Emily Bandel'
+    }
+  }
   render() {
+    //generating local css classes object...
+    var localCSSclasses = {
+      inkstainBackground: 'inkstainmiddle',
+      sectionHeader: 'sectionheader',
+      sectionSubHeader: 'sectionsubheader' 
+    }
+    let {EmilyInfo} = this.props;
+    let sectionName = <h1 className={localCSSclasses.sectionHeader}><span>{this.state.sectionTitle}</span></h1>
+    let sectionMinheader = <h2 className={localCSSclasses.sectionSubHeader}><span>{this.state.sectionDefinition}</span></h2>
     return (
-      <p>**************************************</p>
+      <div className={localCSSclasses.inkstainBackground}>
+      {sectionName}
+      {sectionMinheader}
+      <Row>
+          <Col xs="6" sm="4"><Container>.col-6 .col-sm-4</Container></Col>
+          <Col xs="6" sm="4"><Container>.col-6 .col-sm-4</Container></Col>
+          <Col sm="4"><Container>.col-sm-4</Container></Col>
+        </Row>
+      </div>
+      
     );
   }
 }
