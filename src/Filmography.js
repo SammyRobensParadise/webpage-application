e1102569c82d557df28fd686da6427ff7029078c
SammyRobensParadise <33642354+SammyRobensParadise@users.noreply.github.com>
2018-11-10 18:31:57 -0800

    added the images for the first row of the filmography section

--- src/Filmography.js
+++ src/Filmography.js
@@ -3,6 +3,9 @@ import './App.css';
 import './Filmography.css';
 import App from './App';
 import './assets/inkstainfilm.svg';
+import StandingRoomOnly from './img/standing_room_only.jpg';
+import ATwistedFate from './img/TF_poster.jpg';
+import DeathRomantisized from './img/DR_poster.jpg';
 import { Container, Row, Col } from 'reactstrap';
 
 class Filmography extends Component {
@@ -10,7 +13,12 @@ class Filmography extends Component {
     super(props);
     this.state = {
       sectionTitle: 'Filmography',
-      sectionDefinition: 'A collection of works, films and screenings ft. Emily Bandel'
+      sectionDefinition: 'A collection of works, films and screenings ft. Emily Bandel',
+      images: {
+        StangingRoomCover: StandingRoomOnly,
+        TwistedFateCover: ATwistedFate,
+        DeathRomantisizedCover: DeathRomantisized
+      }
     }
   }
   render() {
@@ -18,19 +26,26 @@ class Filmography extends Component {
     var localCSSclasses = {
       inkstainBackground: 'inkstainmiddle',
       sectionHeader: 'sectionheader',
-      sectionSubHeader: 'sectionsubheader' 
+      sectionSubHeader: 'sectionsubheader',
+      standingRoomOnly: 'standing_room_only_image',
+      aTwistedFate: 'a_twisted_fate',
+      DeathRomantisizedCSS: 'death_romantasized'
     }
     let {EmilyInfo} = this.props;
     let sectionName = <h1 className={localCSSclasses.sectionHeader}><span>{this.state.sectionTitle}</span></h1>
     let sectionMinheader = <h2 className={localCSSclasses.sectionSubHeader}><span>{this.state.sectionDefinition}</span></h2>
+    //generating feature components...
+    let SRObanner = <img src={this.state.images.StangingRoomCover} alt ='Standing Room Only'></img>
+    let TFbanner = <img src={this.state.images.TwistedFateCover} alt="A Twisted Fate"></img>
+    let DRbanner = <img src={this.state.images.DeathRomantisizedCover} alt='Death Romantisized'></img>
     return (
       <div className={localCSSclasses.inkstainBackground}>
       {sectionName}
       {sectionMinheader}
       <Row>
-          <Col xs="6" sm="4"><Container>.col-6 .col-sm-4</Container></Col>
-          <Col xs="6" sm="4"><Container>.col-6 .col-sm-4</Container></Col>
-          <Col sm="4"><Container>.col-sm-4</Container></Col>
+          <Col xs="6" sm="4"><Container><div className={localCSSclasses.standingRoomOnly}>{SRObanner}</div></Container></Col>
+          <Col xs="6" sm="4"><Container><div className={localCSSclasses.aTwistedFate}>{TFbanner}</div></Container></Col>
+          <Col sm="4"><Container><div className={localCSSclasses.DeathRomantisizedCSS}>{DRbanner}</div></Container></Col>
         </Row>
       </div>
       
